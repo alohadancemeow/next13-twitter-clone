68c2e350fecf00993ad8e0596c467903efe8197d
Alohadancemeow <nawongsak@gmail.com>
2023-06-16 16:26:02 +0700

    🐛 fix profile page

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,4 +1,5 @@
 import getCurrentUser from "@/actions/getCurrentUser";
+import getPostsByUserId from "@/actions/getPostsByUserId";
 import getUserById from "@/actions/getUserById";
 import UserProfile from "@/components/UserProfile";
 
@@ -9,11 +10,12 @@ type Props = {
 const page = async ({ params }: { params: Props }) => {
   const { userId } = params;
 
-  // TODO: get user by ID, and current user
+  // TODO: get user by ID, current user, and posts of that user
   const user = await getUserById(userId!);
   const currentUser = await getCurrentUser();
+  const posts = await getPostsByUserId(userId!);
 
-  return <UserProfile user={user} currentUser={currentUser} />;
+  return <UserProfile user={user} currentUser={currentUser} posts={posts} />;
 };
 
 export default page;
--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -6,20 +6,21 @@ import Header from "./Header";
 import UserHero from "./UserHero";
 import UserBio from "./UserBio";
 import PostFeed from "./PostFeed";
-import { UserWithFollowersCount } from "@/types";
+import { PostWithPorps, UserWithFollowersCount } from "@/types";
 
 type Props = {
   user?: UserWithFollowersCount | null;
   currentUser?: User | null;
+  posts?: PostWithPorps[] | null;
 };
 
-const UserProfile = ({ user, currentUser }: Props) => {
+const UserProfile = ({ user, currentUser, posts }: Props) => {
   return (
     <>
       <Header showBackArrow label={user?.name!} />
       <UserHero user={user} />
       <UserBio user={user} currentUser={currentUser} />
-      <PostFeed user={user} currentUser={currentUser} />
+      <PostFeed user={user} currentUser={currentUser} posts={posts}/>
     </>
   );
 };
